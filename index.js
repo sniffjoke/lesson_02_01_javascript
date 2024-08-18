08d63dff4a0e6ea5d1c0926549a7441de93d5936
sniffjoke <sniffjoke@gmail.com>
2024-08-18 14:06:39 +0200

    closures

--- index.js
+++ index.js
@@ -71,6 +71,68 @@
 // couter1()
 // couter1()
 
-couter2()
-couter2()
-couter2()
+// couter2()
+// couter2()
+// couter2()
+
+// ---------------------------------------------------- //
+
+// 2(4) --> 2 * 2(3) --> 2 * 2 * 2(2) --> 2 * 2 * 2 * 2(1)
+
+// const pow = (x, n) => {
+//     if (n === 1) {
+//         return x
+//     } else {
+//         return x * pow(x, n - 1)
+//     }
+// }
+//
+// pow(2, 4) // 16
+//
+// console.log(pow(2, 4))
+
+// --------------------------------------- //
+
+// 5(!) --> 5 * 4(!) --> 5 * 4 * 3(!) --> 5 * 4 * 3 * 2(!) --> 5 * 4 * 3 * 2 * 1(!)
+
+// const factorial = (n) => {
+//     if (n === 1) {
+//         return n
+//     } else {
+//         return n * factorial(n - 1)
+//     }
+// }
+//
+// factorial(5)
+//
+// console.log(factorial(5))
+
+// ----------------------------------------- //
+
+// Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+// function sumTO(n) { /*... ваш код ...*/}
+// console.log(sumTo(100))
+
+// const sumTo = (n) => {
+//     if (n === 1) {
+//         return n;
+//     } else {
+//         return n + sumTo(n - 1)
+//     }
+// }
+//
+// console.log(sumTo(5))
+
+// -------------------------------------------- //
+
+// 321 / 10 = 32.1 (1) --> 32 /10 = 3.2 (2) --> 3/10 --> 3
+
+function genSum(n) {
+    if (n < 10) {
+        return n
+    } else {
+        return (n % 10) + genSum(Math.floor(n/10))
+    }
+}
+
+console.log(genSum(321))
